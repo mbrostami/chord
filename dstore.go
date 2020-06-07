2d39f9899af8f063e44940c8680c51ad64e5e097
Mohamed Rostrami <mrostami@harlemnext.com>
2020-06-08 00:35:53 +0200

    using bbolt

--- dstore.go
+++ dstore.go
@@ -1,22 +1,40 @@
 package chord
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/mbrostami/chord/helpers"
+	bolt "go.etcd.io/bbolt"
 )
 
 // DBREPLICAS this must be less than predecessor list count
 const DBREPLICAS int = 1
+const bucket string = "storage"
 
 type DStore struct {
-	db map[[helpers.HashSize]byte]*[]byte
+	database *bolt.DB
+	db       map[[helpers.HashSize]byte]*[]byte
 }
 
 func NewDStore() *DStore {
+	db, err := bolt.Open("/tmp/chord_"+string(time.Now().Second()), 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(bucket))
+		if err != nil {
+			fmt.Printf("create bucket: %s", err)
+		}
+		return nil
+	})
 	return &DStore{
-		db: make(map[[helpers.HashSize]byte]*[]byte),
+		database: db,
+		db:       make(map[[helpers.HashSize]byte]*[]byte),
 	}
 }
 
@@ -43,7 +61,11 @@ func (d *DStore) Put(value []byte) bool {
 	json, _ := json.Marshal(record)
 	key := helpers.Hash(string(value))
 	// log.Debugf("storing data %x: %v", key, json)
-	d.db[key] = &json
+	d.database.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		err := b.Put(key[:], json)
+		return err
+	})
 	return true
 }
 
@@ -51,35 +73,59 @@ func (d *DStore) PutRecord(record Record) bool {
 	json, _ := json.Marshal(record)
 	key := record.Identifier
 	// log.Debugf("storing data %x: %v", key, json)
-	d.db[key] = &json
+	d.database.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		err := b.Put(key[:], json)
+		return err
+	})
 	return true
 }
 
 func (d *DStore) GetRange(fromKey [helpers.HashSize]byte, toKey [helpers.HashSize]byte) map[[helpers.HashSize]byte]*Record {
 	data := make(map[[helpers.HashSize]byte]*Record)
-	for key, item := range d.db {
-		if helpers.BetweenR(key, fromKey, toKey) {
+	d.database.View(func(tx *bolt.Tx) error {
+		// Assume our events bucket exists and has RFC3339 encoded time keys.
+		c := tx.Bucket([]byte(bucket)).Cursor()
+		// Our time range spans the 90's decade.
+		min := fromKey[:]
+		max := toKey[:]
+		// Iterate over the 90's.
+		for k, value := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, value = c.Next() {
+			var key [helpers.HashSize]byte
+			copy(key[:helpers.HashSize], k[:helpers.HashSize])
 			record := Record{}
-			json.Unmarshal(*item, &record)
+			json.Unmarshal(value, &record)
 			data[key] = &record
 		}
-	}
+		return nil
+	})
 	return data
 }
 
 func (d *DStore) Get(key [helpers.HashSize]byte) []byte {
-	if val, ok := d.db[key]; ok {
-		return *val
-	}
-	return nil
+	var result []byte
+	d.database.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		result = b.Get(key[:])
+		return nil
+	})
+	return result
 }
 
 func (d *DStore) GetAll() map[[helpers.HashSize]byte]*Record {
 	data := make(map[[helpers.HashSize]byte]*Record)
-	for key, item := range d.db {
-		record := Record{}
-		json.Unmarshal(*item, &record)
-		data[key] = &record
-	}
+	d.database.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket([]byte(bucket))
+		c := b.Cursor()
+		for k, value := c.First(); k != nil; k, value = c.Next() {
+			var key [helpers.HashSize]byte
+			copy(key[:helpers.HashSize], k[:helpers.HashSize])
+			record := Record{}
+			json.Unmarshal(value, &record)
+			data[key] = &record
+		}
+		return nil
+	})
 	return data
 }
--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/matryer/moq v0.0.0-20200310130814-7721994d1b54 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/sirupsen/logrus v1.6.0
+	go.etcd.io/bbolt v1.3.4
 	google.golang.org/grpc v1.29.1
 )
